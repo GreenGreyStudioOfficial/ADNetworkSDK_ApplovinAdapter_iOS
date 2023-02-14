520946a36be38dc35f4347257ec511ea1645f4a1
Александр Троицкий <alexander.troitsky@gmail.com>
2023-02-14 22:36:31 +0300

    added applovin depending

--- Package.swift
+++ Package.swift
@@ -12,9 +12,24 @@ let package = Package(
             targets: ["ADNetworkSDK_ApplovinAdapter"]
         )
     ],
+    dependencies: [
+        .appLovin
+    ],
     targets: [
         .target(
-            name: "ADNetworkSDK_ApplovinAdapter"
+            name: "ADNetworkSDK_ApplovinAdapter",
+            dependencies: [
+                .AppLovinSDK
+            ]
         )
     ]
 )
+
+extension Package.Dependency {
+    static let  appLovin: Package.Dependency =
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: Version("11.5.0"))
+}
+
+extension Target.Dependency {
+    static let AppLovinSDK: Target.Dependency = .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package")
+}
