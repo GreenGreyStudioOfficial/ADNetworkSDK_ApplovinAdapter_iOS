6eab727b7d784c94d4841fcb9d2c3a7ce4c480d4
Александр Троицкий <alexander.troitsky@gmail.com>
2023-02-14 18:13:09 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ADNetworkSDK_ApplovinAdapter
+
+A description of this package.
---
+++ Sources/ADNetworkSDK_ApplovinAdapter/ADNetworkSDK_ApplovinAdapter.swift
@@ -0,0 +1,6 @@
+public struct ADNetworkSDK_ApplovinAdapter {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
